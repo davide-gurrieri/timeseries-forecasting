cc7bbf19b9d13f7cda0607cf0b8155407db07162
sabrinanegroni <negroni.sabrina@gmail.com>
2023-12-20 11:24:00 +0100

    fixed

--- models/ConvLSTMDenseAttentionConcat.py
+++ models/ConvLSTMDenseAttentionConcat.py
@@ -42,7 +42,7 @@ class ConvLSTMDenseAttentionConcat(GeneralModel):
     def build(self):
         tf.random.set_seed(self.seed)
 
-        relu_init = tfk.initializers.HeUniform(seed=self.seed)
+        #relu_init = tfk.initializers.HeUniform(seed=self.seed)
 
         input_layer = tfkl.Input(shape=self.build_kwargs["input_shape"], name="Input")
         
@@ -56,15 +56,13 @@ class ConvLSTMDenseAttentionConcat(GeneralModel):
         # Concatenate the attention output with the LSTM output
         attended_output = tfkl.Concatenate(axis=-1, name='concatenate_attention')([x, attention])
 
-        x = tfkl.Conv1D(32, 3, padding='same', activation='relu', kernel_initializer=relu_init,
-         name='conv')(attended_output)
+        x = tfkl.Conv1D(32, 3, padding='same', activation='relu', name='conv')(attended_output)
         
         x = tfkl.Flatten()(x)
 
         output_layer = tfkl.Dense(
             units=self.build_kwargs["output_shape"],
             activation="relu",
-            kernel_initializer=relu_init,
             name="Output",
         )(x)
 
