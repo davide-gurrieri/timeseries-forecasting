ca91b99a651ae609eef937e2b785a62e85e6714c
sabrinanegroni <negroni.sabrina@gmail.com>
2023-12-21 09:35:41 +0100

    add DoubleBidirConvDropBN

--- models/DoubleBidirConvDropBN.py
+++ models/DoubleBidirConvDropBN.py
@@ -11,7 +11,7 @@ build_param_1 = {
 compile_param_1 = {
     "loss": tfk.losses.MeanSquaredError(),
     "optimizer": tfk.optimizers.Adam(learning_rate=0.001),
-    "metrics": ['mae'],
+    "metrics":['mae'],
 }
 
 fit_param_1 = {
@@ -35,48 +35,42 @@ class DoubleBidirConvDropBN(GeneralModel):
         self.name = name
 
     def build(self):
-    tf.random.set_seed(self.seed)
+        tf.random.set_seed(self.seed)
 
-    input_layer = tfkl.Input(shape=self.build_kwargs["input_shape"], name="Input")
-    
-    x = DataAugmentation(prob=0.5, min_sigma=0.015, max_sigma=0.03)(input_layer)
-    
-    x = tfkl.Dropout(0.1)(x)
-    
-    x = tfkl.Conv1D(128, 3, padding='same', activation='relu', name='conv_0')(x)
-    x = tfkl.BatchNormalization()(x)  
-    #x = tfkl.Dropout(0.5)(x)  
-    
-    x = tfkl.Bidirectional(tfkl.LSTM(64, return_sequences=True, name='lstm_1'), name='bidirectional_lstm')(x)
-    x = tfkl.BatchNormalization()(x)  
-    
-    x = tfkl.Conv1D(64, 3, padding='same', activation='relu', name='conv_1')(x)
-    x = tfkl.BatchNormalization()(x)  
-    
-    x = tfkl.Bidirectional(tfkl.LSTM(64, return_sequences=True, name='lstm_2'), name='bidirectional_lstm')(x)
-    x = tfkl.BatchNormalization()(x)  
-    
-    x = tfkl.Conv1D(128, 3, padding='same', activation='relu', name='conv_2')(x)
-    x = tfkl.BatchNormalization()(x)  
-    
-    x = tfkl.Conv1D(64, 3, padding='same', activation='relu', name='conv_3')(x)
-    x = tfkl.BatchNormalization()(x)  
-    
-    x = tfkl.Flatten()(x)
-    
-    x = tfkl.Dropout(0.1)(x)
-    
-    output_layer = tfkl.Dense(
-        units=self.build_kwargs["output_shape"],
-        activation="sigmoid",
-        name="Output",
-    )(x)
+        input_layer = tfkl.Input(shape=self.build_kwargs["input_shape"], name="Input")
 
-    return tf.keras.Model(inputs=input_layer, outputs=output_layer)
+        x = DataAugmentation(prob=0.5, min_sigma=0.015, max_sigma=0.03)(input_layer)
 
+        x = tfkl.Dropout(0.1)(x)
 
-    return tf.keras.Model(inputs=input_layer, outputs=output_layer)
+        x = tfkl.Conv1D(128, 3, padding='same', activation='relu', name='conv_0')(x)
+        x = tfkl.BatchNormalization()(x)  
+        #x = tfkl.Dropout(0.5)(x)  
 
+        x = tfkl.Bidirectional(tfkl.LSTM(64, return_sequences=True, name='lstm_1'), name='bidirectional_lstm')(x)
+        x = tfkl.BatchNormalization()(x)  
+
+        x = tfkl.Conv1D(64, 3, padding='same', activation='relu', name='conv_1')(x)
+        x = tfkl.BatchNormalization()(x)  
+
+        x = tfkl.Bidirectional(tfkl.LSTM(64, return_sequences=True, name='lstm_2'), name='bidirectional_lstm')(x)
+        x = tfkl.BatchNormalization()(x)  
+
+        x = tfkl.Conv1D(128, 3, padding='same', activation='relu', name='conv_2')(x)
+        x = tfkl.BatchNormalization()(x)  
+
+        x = tfkl.Conv1D(64, 3, padding='same', activation='relu', name='conv_3')(x)
+        x = tfkl.BatchNormalization()(x)  
+
+        x = tfkl.Flatten()(x)
+
+        x = tfkl.Dropout(0.1)(x)
+
+        output_layer = tfkl.Dense(
+            units=self.build_kwargs["output_shape"],
+            activation="sigmoid",
+            name="Output",
+        )(x)
 
         # Connect input and output through the Model class
         self.model = tfk.Model(inputs=input_layer, outputs=output_layer, name=self.name)
